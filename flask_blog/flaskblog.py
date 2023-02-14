e16ef5dfbb0e777b1bf9383562712c62edfea327
Igor <ipovaric@gmail.com>
2023-02-13 21:43:13 -0700

    fix: resolved issue of creating sqlite db in flask
    I had to add a line that would manually push the application context
    out to the command line in order to allow flask to run the
    db.create_all() and other subsequent commands from the command-line
    python instance.

--- flask_blog/flaskblog.py
+++ flask_blog/flaskblog.py
@@ -8,8 +8,9 @@ app.config['SECRET_KEY'] = '389106e0b2193996d6b690826ebf1dea'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
-with app.app_context():
-    db.create_all()
+# Igor: I had to add this line to make sure flask had the application context in order
+#   to run some commands from the commandline python.
+app.app_context().push()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
