04ed3b25c8efab8853b8592697dbd60413082520
Igor <ipovaric@gmail.com>
2023-02-13 23:08:57 -0700

    user sessions and issue with redirect
    It's supposed to redirect to home when a valid user is present in the
    current session, but it's returning a False in the current_user.
    is_authenticated field.

--- flask_blog/flask_blog/__init__.py
+++ flask_blog/flask_blog/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '389106e0b2193996d6b690826ebf1dea'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from flask_blog import routes
 
