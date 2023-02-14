bd15f6bf9a8f7ba991b571c8df1f45609cd66a05
Igor <ipovaric@gmail.com>
2023-02-13 22:49:27 -0700

    pw hash codes and registration screen validation

--- flask_blog/flask_blog/__init__.py
+++ flask_blog/flask_blog/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '389106e0b2193996d6b690826ebf1dea'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flask_blog import routes
 
